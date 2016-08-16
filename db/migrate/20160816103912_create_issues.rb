02960fe4b2575f5dc5db9a616f30b982a77d35ea
Jenua Boiko <jeyboy@bigmir.net>
2016-08-16 13:57:22 +0300

    configuration

--- db/migrate/20160816103912_create_issues.rb
+++ db/migrate/20160816103912_create_issues.rb
@@ -1,6 +1,7 @@
 class CreateIssues < ActiveRecord::Migration[5.0]
   def change
     create_table :issues do |t|
+      t.string :project_name
       t.string :title
       t.text :body
 
--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,11 @@ ActiveRecord::Schema.define(version: 20160816103912) do
   enable_extension "plpgsql"
 
   create_table "issues", force: :cascade do |t|
+    t.string   "project_name"
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
